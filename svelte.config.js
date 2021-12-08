a4af1dad36519c5986c53ebacb78ea0cdb209978
Robson Ramos <rpbramos@gmail.com>
2021-12-08 13:43:43 -0300

    initial docs commit

--- svelte.config.js
+++ svelte.config.js
@@ -22,14 +22,14 @@ const config = {
 	kit: {
 		adapter: adapter({
 			// default options are shown
-			pages: 'site',
-			assets: 'site',
+			pages: 'docs',
+			assets: 'docs',
 			fallback: null
 		}),
 		appDir: '.',
 		paths: {
 			// base: dev ? '' : '/relicjs.github.io',
-			base: '/site'
+			base: '/docs'
 		},
 
 		// hydrate the <div id="svelte"> element in src/app.html
