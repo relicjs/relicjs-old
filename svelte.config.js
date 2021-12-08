44985a61403e09751fa69066423086d4f5a5a806
Robson Ramos <rpbramos@gmail.com>
2021-12-08 10:56:36 -0300

    updated docs website

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = process.env.NODE_ENV === 'development';
+// const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -26,9 +26,9 @@ const config = {
 			assets: 'build',
 			fallback: null
 		}),
-		paths: {
-			base: dev ? '' : '/relicjs.github.io',
-		},
+		// paths: {
+		// 	base: dev ? '' : '/relicjs.github.io',
+		// },
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
